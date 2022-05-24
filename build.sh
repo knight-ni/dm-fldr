e1b84e725e4f2c0c2b205c8a7255355df78dfefc
Knight.Ni <knightnibo@gmail.com>
2022-05-24 11:52:21 +0800

    thread version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
 DM_HOME=/dm8
-gcc -o test test.c -I ${DM_HOME}/drivers/fldr/include -L ${DM_HOME}/drivers/fldr -ldmfldr
+gcc -o dm_fldr_with_thread dm_fldr_with_thread.c -I ${DM_HOME}/drivers/fldr/include -L ${DM_HOME}/drivers/fldr -ldmfldr -lpthread
